88a8f58bb358d642b7a486cce1fada513d64ed41
chris <chegechris959@gmail.com>
2023-07-13 16:31:55 -0700

    first c prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
+/* author: "chris"
+ *
+ */
+
+
 #include <stdio.h>
 
 int main(void)
+
+	/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
