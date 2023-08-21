b40906880bc6e96c3df7dbbbb76bcdc49699a20b
chris <chegechris959@gmail.com>
2023-08-21 05:51:59 -0700

    siie

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - calculates lenght of a string
+ * @s: char target
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int cnt, in;
+
+	in = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	inc++;
+
+return (inc);
+}
